a199603102161e6094334bd6cba54f1b941fd037
muguremre <muhammeduguremre@gmail.com>
2023-12-25 07:17:49 +0300

    main güncellendi

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:dogus_app/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
